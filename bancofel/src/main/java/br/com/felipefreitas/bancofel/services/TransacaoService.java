90e50597623dfb1f3109e6be350bfd6ecbad1121
felipe.freitas <felipe.freitas@foursys.com.br>
2026-07-01 15:50:04 -0300

    Atualizando ContaService adicionando TransacaoService

--- bancofel/src/main/java/br/com/felipefreitas/bancofel/services/TransacaoService.java
+++ bancofel/src/main/java/br/com/felipefreitas/bancofel/services/TransacaoService.java
@@ -11,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @Service
